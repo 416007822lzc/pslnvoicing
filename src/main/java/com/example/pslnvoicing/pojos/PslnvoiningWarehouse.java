a2d6175c169d04869bdaca3ec671d6e6d57738b5
TiAmo <1218004421@qq.com>
2021-04-03 16:30:45 +0800

    第一次提交 LWB

---
+++ src/main/java/com/example/pslnvoicing/pojos/PslnvoiningWarehouse.java
@@ -0,0 +1,128 @@
+package com.example.pslnvoicing.pojos;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+public class PslnvoiningWarehouse {
+	private String warehouseName;
+	private int warehouseId;
+	private int warehouseNum;
+	private String warehouseAddress;
+	private String warehouseHead;
+	private int warehousePhone;
+	private int warehouseCall;
+	private String warehouseNote;
+	private int warehouseDisable;
+
+	@Basic
+	@Column(name = "warehouse_name")
+	public String getWarehouseName() {
+		return warehouseName;
+	}
+
+	public void setWarehouseName(String warehouseName) {
+		this.warehouseName = warehouseName;
+	}
+
+	@Id
+	@Column(name = "warehouse_id")
+	public int getWarehouseId() {
+		return warehouseId;
+	}
+
+	public void setWarehouseId(int warehouseId) {
+		this.warehouseId = warehouseId;
+	}
+
+	@Basic
+	@Column(name = "warehouse_num")
+	public int getWarehouseNum() {
+		return warehouseNum;
+	}
+
+	public void setWarehouseNum(int warehouseNum) {
+		this.warehouseNum = warehouseNum;
+	}
+
+	@Basic
+	@Column(name = "warehouse_address")
+	public String getWarehouseAddress() {
+		return warehouseAddress;
+	}
+
+	public void setWarehouseAddress(String warehouseAddress) {
+		this.warehouseAddress = warehouseAddress;
+	}
+
+	@Basic
+	@Column(name = "warehouse_head")
+	public String getWarehouseHead() {
+		return warehouseHead;
+	}
+
+	public void setWarehouseHead(String warehouseHead) {
+		this.warehouseHead = warehouseHead;
+	}
+
+	@Basic
+	@Column(name = "warehouse_phone")
+	public int getWarehousePhone() {
+		return warehousePhone;
+	}
+
+	public void setWarehousePhone(int warehousePhone) {
+		this.warehousePhone = warehousePhone;
+	}
+
+	@Basic
+	@Column(name = "warehouse_call")
+	public int getWarehouseCall() {
+		return warehouseCall;
+	}
+
+	public void setWarehouseCall(int warehouseCall) {
+		this.warehouseCall = warehouseCall;
+	}
+
+	@Basic
+	@Column(name = "warehouse_note")
+	public String getWarehouseNote() {
+		return warehouseNote;
+	}
+
+	public void setWarehouseNote(String warehouseNote) {
+		this.warehouseNote = warehouseNote;
+	}
+
+	@Basic
+	@Column(name = "warehouse_disable")
+	public int getWarehouseDisable() {
+		return warehouseDisable;
+	}
+
+	public void setWarehouseDisable(int warehouseDisable) {
+		this.warehouseDisable = warehouseDisable;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		PslnvoiningWarehouse that = (PslnvoiningWarehouse) o;
+		return warehouseId == that.warehouseId &&
+				warehouseNum == that.warehouseNum &&
+				warehousePhone == that.warehousePhone &&
+				warehouseCall == that.warehouseCall &&
+				warehouseDisable == that.warehouseDisable &&
+				Objects.equals(warehouseName, that.warehouseName) &&
+				Objects.equals(warehouseAddress, that.warehouseAddress) &&
+				Objects.equals(warehouseHead, that.warehouseHead) &&
+				Objects.equals(warehouseNote, that.warehouseNote);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(warehouseName, warehouseId, warehouseNum, warehouseAddress, warehouseHead, warehousePhone, warehouseCall, warehouseNote, warehouseDisable);
+	}
+}
---
+++ src/main/java/com/example/pslnvoicing/pojos/PurchaseReturndetails.java
@@ -0,0 +1,104 @@
+package com.example.pslnvoicing.pojos;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+public class PurchaseReturndetails {
+	private int pRdId;
+	private Integer pRId;
+	private String pRdName;
+	private Integer pRdNumber;
+	private String pRdCompany;
+	private Double pRdUnitprice;
+	private Double pRdTotalprice;
+
+	@Id
+	@Column(name = "p_rd_id")
+	public int getpRdId() {
+		return pRdId;
+	}
+
+	public void setpRdId(int pRdId) {
+		this.pRdId = pRdId;
+	}
+
+	@Basic
+	@Column(name = "p_r_id")
+	public Integer getpRId() {
+		return pRId;
+	}
+
+	public void setpRId(Integer pRId) {
+		this.pRId = pRId;
+	}
+
+	@Basic
+	@Column(name = "p_rd_name")
+	public String getpRdName() {
+		return pRdName;
+	}
+
+	public void setpRdName(String pRdName) {
+		this.pRdName = pRdName;
+	}
+
+	@Basic
+	@Column(name = "p_rd_number")
+	public Integer getpRdNumber() {
+		return pRdNumber;
+	}
+
+	public void setpRdNumber(Integer pRdNumber) {
+		this.pRdNumber = pRdNumber;
+	}
+
+	@Basic
+	@Column(name = "p_rd_company")
+	public String getpRdCompany() {
+		return pRdCompany;
+	}
+
+	public void setpRdCompany(String pRdCompany) {
+		this.pRdCompany = pRdCompany;
+	}
+
+	@Basic
+	@Column(name = "p_rd_unitprice")
+	public Double getpRdUnitprice() {
+		return pRdUnitprice;
+	}
+
+	public void setpRdUnitprice(Double pRdUnitprice) {
+		this.pRdUnitprice = pRdUnitprice;
+	}
+
+	@Basic
+	@Column(name = "p_rd_totalprice")
+	public Double getpRdTotalprice() {
+		return pRdTotalprice;
+	}
+
+	public void setpRdTotalprice(Double pRdTotalprice) {
+		this.pRdTotalprice = pRdTotalprice;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		PurchaseReturndetails that = (PurchaseReturndetails) o;
+		return pRdId == that.pRdId &&
+				Objects.equals(pRId, that.pRId) &&
+				Objects.equals(pRdName, that.pRdName) &&
+				Objects.equals(pRdNumber, that.pRdNumber) &&
+				Objects.equals(pRdCompany, that.pRdCompany) &&
+				Objects.equals(pRdUnitprice, that.pRdUnitprice) &&
+				Objects.equals(pRdTotalprice, that.pRdTotalprice);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(pRdId, pRId, pRdName, pRdNumber, pRdCompany, pRdUnitprice, pRdTotalprice);
+	}
+}
