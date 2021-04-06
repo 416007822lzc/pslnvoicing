334e7910c2aa3e28d6179a719d7543dbbcb64b2a
41600 <416007822@qq.com>
2021-04-06 19:47:40 +0800

    0406.2登录，审批整合2

--- src/main/java/com/example/pslnvoicing/mapper/salemapper/CustomerMapper.java
+++ src/main/java/com/example/pslnvoicing/mapper/salemapper/CustomerMapper.java
@@ -1,12 +1,13 @@
 package com.example.pslnvoicing.mapper.salemapper;
 
 import com.example.pslnvoicing.pojos.PslnvoicingClient;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
-@Repository
+@Mapper
 public interface CustomerMapper {
     List<PslnvoicingClient> pslnvoicingClientList();
 }
